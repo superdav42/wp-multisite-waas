0f15842aea51fed881becddde76ff8d2ea6f0e34
Patrick Groot <info@patrickgroot.com>
2025-04-19 00:48:56 +0200

    [55] command timeout bump

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   video: false,
   e2e: {
     baseUrl: "http://localhost:8889",
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 20000,
     specPattern: "tests/e2e/cypress/integration/**/*.{js,jsx,ts,tsx}",
     supportFile: "tests/e2e/cypress/support/e2e.js",
     setupNodeEvents(on, config) {
