543f859e9ebaadfdb2d05ca8fe47d2e69b9eff4d
Patrick Groot <info@patrickgroot.com>
2025-04-21 19:57:36 +0200

    [55] Update test

--- tests/e2e/cypress/integration/plugin.spec.js
+++ tests/e2e/cypress/integration/plugin.spec.js
@@ -20,13 +20,4 @@ describe("Plugin", () => {
     cy.location("pathname").should("eq", "/wp-admin/network/admin.php");
     cy.location("search").should("include", "page=wp-ultimo-setup");
   });
-
-  it("Should show an error message that the plugin needs to be network activated", () => {
-    cy.visit("/wp-admin/");
-    cy.get(".notice.notice-error")
-      .contains(
-        'WP Multisite WaaS needs to be network active to run properly. You can "Network Activate" it here'
-      )
-      .should("be.visible");
-  });
 });
