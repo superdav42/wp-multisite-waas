a80f4258e9ae6b2f0fa7de85ad932dd161eab050
Patrick Groot <info@patrickgroot.com>
2025-04-21 17:15:52 +0200

    [55] Fix tests

--- tests/e2e/cypress/integration/plugin.spec.js
+++ tests/e2e/cypress/integration/plugin.spec.js
@@ -6,27 +6,28 @@ describe("Plugin", () => {
     );
   });
 
-  it.skip("Should show an error message that the plugin needs to be network activated", () => {
-    cy.visit("/wp-admin/");
-    cy.get(".notice.notice-error")
-      .contains(
-        'WP Multisite WaaS needs to be network active to run properly. You can "Network Activate" it here'
-      )
-      .should("be.visible");
+  it("Should be able to deactivate the plugin", () => {
+    cy.visit("/wp-admin/network/plugins.php");
+    cy.location("pathname").should("equal", "/wp-admin/network/plugins.php");
+    cy.get("#deactivate-wp-multisite-waas").scrollIntoView().should("be.visible").click();
+    cy.get("#activate-wp-multisite-waas").scrollIntoView().should("be.visible");
   });
 
   it("Should be able to activate the plugin", () => {
     cy.visit("/wp-admin/network/plugins.php");
     cy.location("pathname").should("equal", "/wp-admin/network/plugins.php");
     cy.get("#activate-wp-multisite-waas").scrollIntoView().should("be.visible").click();
+    cy.get("#deactivate-wp-multisite-waas").scrollIntoView().should("be.visible");
     cy.location("pathname").should("eq", "/wp-admin/network/admin.php");
     cy.location("search").should("include", "page=wp-ultimo-setup");
   });
 
-  it("Should be able to deactivate the plugin", () => {
-    cy.visit("/wp-admin/network/plugins.php");
-    cy.location("pathname").should("equal", "/wp-admin/network/plugins.php");
-    cy.get("#deactivate-wp-multisite-waas").scrollIntoView().should("be.visible").click();
-    cy.get("#activate-wp-multisite-waas").scrollIntoView().should("be.visible");
+  it("Should show an error message that the plugin needs to be network activated", () => {
+    cy.visit("/wp-admin/");
+    cy.get(".notice.notice-error")
+      .contains(
+        'WP Multisite WaaS needs to be network active to run properly. You can "Network Activate" it here'
+      )
+      .should("be.visible");
   });
 });
