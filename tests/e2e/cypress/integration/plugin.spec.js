b9c9c72c9fddf6d6914b955b9f87333b6512b962
Patrick Groot <info@patrickgroot.com>
2025-07-25 15:57:01 +0200

    Bump WP and fix test

--- tests/e2e/cypress/integration/plugin.spec.js
+++ tests/e2e/cypress/integration/plugin.spec.js
@@ -9,14 +9,14 @@ describe("Plugin", () => {
   it("Should be able to deactivate the plugin", () => {
     cy.visit("/wp-admin/network/plugins.php");
     cy.location("pathname").should("equal", "/wp-admin/network/plugins.php");
-    cy.get("#deactivate-wp-multisite-waas").scrollIntoView().should("be.visible").click();
-    cy.get("#activate-wp-multisite-waas").scrollIntoView().should("be.visible");
+    cy.get("#deactivate-multisite-ultimate").scrollIntoView().should("be.visible").click();
+    cy.get("#activate-multisite-ultimate").scrollIntoView().should("be.visible");
   });
 
   it("Should be able to activate the plugin", () => {
     cy.visit("/wp-admin/network/plugins.php");
     cy.location("pathname").should("equal", "/wp-admin/network/plugins.php");
-    cy.get("#activate-wp-multisite-waas").scrollIntoView().should("be.visible").click();
+    cy.get("#activate-multisite-ultimate").scrollIntoView().should("be.visible").click();
     cy.location("pathname").should("eq", "/wp-admin/network/admin.php");
     cy.location("search").should("include", "page=wp-ultimo-setup");
   });
