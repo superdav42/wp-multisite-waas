0937fc916f1e6eb8d723b6425d0e7d9536f27764
Patrick Groot <info@patrickgroot.com>
2025-08-03 22:51:16 +0200

    Updating scripts and configuration to make sure tests run, environment needs to be cleanly rebuild since cleaning doesn't cut it

--- cypress.config.js
+++ cypress.config.js
@@ -8,21 +8,19 @@ module.exports = defineConfig({
   downloadsFolder: "tests/e2e/cypress/downloads",
   screenshotsFolder: "tests/e2e/cypress/screenshots",
   videosFolder: "tests/e2e/cypress/videos",
-  retries: {
-    runMode: 1,
-    openMode: 0
-  },
   video: true,
+  retries: {
+      runMode: 1,
+      openMode: 0
+  },
   e2e: {
+    specPattern: "tests/e2e/cypress/integration/**/*.{js,jsx,ts,tsx}",
+    supportFile: "tests/e2e/cypress/support/e2e.js",
     baseUrl: "http://localhost:8889",
     defaultCommandTimeout: 20000,
     requestTimeout: 30000,
     responseTimeout: 30000,
     pageLoadTimeout: 60000,
-    specPattern: "tests/e2e/cypress/integration/**/*.{js,jsx,ts,tsx}",
-    supportFile: "tests/e2e/cypress/support/e2e.js",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
