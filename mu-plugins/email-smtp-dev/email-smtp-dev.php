784cd146229d9fed53c7175481c4625619cc773c
Patrick Groot <info@patrickgroot.com>
2025-08-08 19:52:49 +0200

    Update mu-plugins

--- mu-plugins/email-smtp-dev/email-smtp-dev.php
+++ mu-plugins/email-smtp-dev/email-smtp-dev.php
@@ -10,12 +10,12 @@ add_action('phpmailer_init', function($phpmailer) {
     error_log('📬 PHPMailer init hook triggered');
 
     $phpmailer->isSMTP();
-    $phpmailer->Host       = 'mailpit';     // Mailpit SMTP host
-    $phpmailer->Port       = 1025;          // Mailpit SMTP port
-    $phpmailer->SMTPAuth   = false;         // No auth for Mailpit by default
-    $phpmailer->SMTPSecure = false;         // No encryption for local SMTP
-    $phpmailer->Username   = '';            // Leave empty
-    $phpmailer->Password   = '';            // Leave empty
+    $phpmailer->Host       = 'wp-multisite-waas-mailpit'; // Mailpit SMTP host
+    $phpmailer->Port       = 1025;                        // Mailpit SMTP port
+    $phpmailer->SMTPAuth   = false;                       // No auth for Mailpit by default
+    $phpmailer->SMTPSecure = false;                       // No encryption for local SMTP
+    $phpmailer->Username   = '';                          // Leave empty
+    $phpmailer->Password   = '';                          // Leave empty
 
     // Optional: set the default From address and name for all outgoing emails
     $phpmailer->From       = 'dev@example.local';
