11f78e2187560240a78c6199eb0a91a4639bd258
Patrick Groot <info@patrickgroot.com>
2025-08-08 21:30:14 +0200

    fix-phpmyadmin-pma-hosts

--- bin/phpmyadmin.sh
+++ bin/phpmyadmin.sh
@@ -2,7 +2,7 @@
 docker run -d \
   --name "${3}-phpmyadmin" \
   -p 8080:80 \
-  -e PMA_HOSTS="${2}-mysql-1,${2}tests-mysql-1" \
+  -e PMA_HOSTS="${2}-mysql-1,${2}-tests-mysql-1" \
   -e PMA_VERBOSES="Development,Test" \
   -e PMA_USER=root \
   -e PMA_PASSWORD=password \
