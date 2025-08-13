3dc4fbb6be66a052dd9ad107f3c7290354eae9de
David Stone <david@nnucomputerwhiz.com>
2025-08-13 13:26:43 -0600

    Add doing it wrong

--- inc/ui/class-current-site-element.php
+++ inc/ui/class-current-site-element.php
@@ -354,6 +354,7 @@ class Current_Site_Element extends Base_Element {
 
 		// Defensive check - setup() may have been called but site can still be null
 		if ( ! $this->site) {
+			_doing_it_wrong(__METHOD__, esc_html__('setup() or setup_preview() must be called before output().', 'multisite-ultimate'));
 			return '';
 		}
 
